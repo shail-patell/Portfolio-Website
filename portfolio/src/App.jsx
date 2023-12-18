96a40b9d0e2dcac0146d5a0f0ed63061ab550db9
Shail Patel <shailpatel@Shails-MBP.ht.home>
2023-12-18 13:50:27 -0500

    first commit

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,35 @@
+import Header from "./components/Header"
+import './App.css'
+const App = () => {
+  return (
+    <>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="toggler-icon top-bar"></span>
+      <span class="toggler-icon middle-bar"></span>
+      <span class="toggler-icon bottom-bar"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">About Me</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Portfolio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact Me</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+      <Header text="Hi there, I'm Shail!"/>
+      <h2 id='subtext' className="text-center">I love coding, solving problems, and building cool things!</h2>
+    </>
+  )
+}
+
+export default App
---
+++ portfolio/src/components/Header.jsx
@@ -0,0 +1,8 @@
+import './HeaderStyle.css'
+
+const About = ({text}) => {
+    return (
+        <h1 id='header' className="p-5 display-3 text-center">{text}</h1>
+    )
+}
+export default About
